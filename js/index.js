15a6262725ab7847b49dc1738f7829ce8b219e1a
Mostafa Hagag <mostafahagag54@gmail.com>
2022-12-26 21:53:31 +0200

    fristCommit

---
+++ js/index.js
@@ -0,0 +1,25 @@
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+let MeterToFeet= document.getElementById("metertoFeet");
+let LitersToGalons=document.getElementById("litersToGallon");
+let KiloGramToPound=document.getElementById("kiloToPounds");
+let convButton= document.getElementById("conv")
+function convertMeterToFeet(){
+    MeterToFeet.innerHTML = `20 metres = ${20*3.281} | 20 feet = 6.069 `
+}
+function convertLitersToGalons(){
+    LitersToGalons.innerHTML = `20 metres = ${20*3.281} | 20 feet = 6.069 `
+}
+function convertKiloGramToPound(){
+    KiloGramToPound.innerHTML = `20 metres = ${20*3.281} | 20 feet = 6.069 `
+}
+
+convButton.addEventListener("click",function (){
+    
+     convertMeterToFeet()
+     convertKiloGramToPound()
+     convertLitersToGalons()
+})
